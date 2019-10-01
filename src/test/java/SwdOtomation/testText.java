c6450adb98dd9c5c97bb1f6f865417d0a7780b00
ikibas <ikibas@gmail.com>
2019-10-01 09:20:26 -0500

    cevo branch

--- src/test/java/SwdOtomation/testText.java
+++ src/test/java/SwdOtomation/testText.java
@@ -29,37 +29,37 @@ public class testText extends Base{
 		lggr.info("driver is initial,zed");
 		driver.get(prop.getProperty("url"));
 		lggr.info("driver is initial,zed");
-		
+
 	}
 	@Test
 	public void basePageNavigation() throws IOException
 	{
 		 ExtentHtmlReporter reporter=new ExtentHtmlReporter("C://test//test1.html");
-			
+
          // Create object of ExtentReports class- This is main class which will create report
 	    ExtentReports extent = new ExtentReports();
-	    
+
          // attach the reporter which we created in Step 1
 	    extent.attachReporter(reporter);
-	    
+
          // call createTest method and pass the name of TestCase- Based on your requirement
 	    ExtentTest logger=extent.createTest("zzzLoginTest");
-	    
+
          // log method will add logs in report and provide the log steps which will come in report
 	    logger.log(Status.INFO, "Login to amazon");
-	   
+
 	    logger.log(Status.PASS, "Title verified");
-	   
-         // Flush method will write the test in report- This is mandatory step  
-	  
-		
-		
+
+         // Flush method will write the test in report- This is mandatory step
+
+
+
 		// access to object
 		//1. extends
 		//2. create object
-		
+
 		 lp = new landingPage(driver);
-		
+
 		//System.out.println(lp.getText1().getText());
 		Assert.assertEquals("FEATURED COURSES123", lp.getText1().getText());
 		//Assert.assertTrue(lp.getText1().getText().equals("abcd"));
@@ -68,42 +68,47 @@ public class testText extends Base{
 		//lop.getEmail().sendKeys(username);
 		//lop.getPassword().sendKeys(password);
 		//System.out.println(text);
+
+		System.out.println("text added");
+			System.out.println("text added");
+				System.out.println("text added");
+					System.out.println("text added");
 		//lop.getSignin().click();
 		logger.log(Status.SKIP, "test skipped");
 		//lggr.info("successfully valideted");
 		  extent.flush();
-		
+
 	}
-	
+
 	@Test
 	public void headerTest() throws IOException
 	{
 		 ExtentHtmlReporter reporter=new ExtentHtmlReporter("C://test//test1.html");
-			
+
          // Create object of ExtentReports class- This is main class which will create report
 	    ExtentReports extent = new ExtentReports();
-	    
+
          // attach the reporter which we created in Step 1
 	    extent.attachReporter(reporter);
-	    
+
          // call createTest method and pass the name of TestCase- Based on your requirement
 	    ExtentTest logger=extent.createTest("zzzLoginTest");
-	    
+
          // log method will add logs in report and provide the log steps which will come in report
 	    logger.log(Status.INFO, "Login to amazon");
-	   
+
 	    logger.log(Status.PASS, "Title verified");
-	   
-         // Flush method will write the test in report- This is mandatory step  
-	  
-		
-		
+
+         // Flush method will write the test in report- This is mandatory step
+
+
+
 		// access to object
 		//1. extends
 		//2. create object
-		
+
 	lp = new landingPage(driver);
-		
+
 		//System.out.println(lp.getText1().getText());
 		Assert.assertEquals("aaaa", lp.getText2().getText());
 		//Assert.assertTrue(lp.getText1().getText().equals("abcd"));
@@ -116,37 +121,37 @@ public class testText extends Base{
 		logger.log(Status.SKIP, "test skipped");
 		//lggr.info("successfully valideted");
 		  extent.flush();
-		
+
 	}
 	@Test
 	public void headerTest1() throws IOException
 	{
 		 ExtentHtmlReporter reporter=new ExtentHtmlReporter("C://test//test1.html");
-			
+
          // Create object of ExtentReports class- This is main class which will create report
 	    ExtentReports extent = new ExtentReports();
-	    
+
          // attach the reporter which we created in Step 1
 	    extent.attachReporter(reporter);
-	    
+
          // call createTest method and pass the name of TestCase- Based on your requirement
 	    ExtentTest logger=extent.createTest("zzzLoginTest");
-	    
+
          // log method will add logs in report and provide the log steps which will come in report
 	    logger.log(Status.INFO, "Login to amazon");
-	   
+
 	    logger.log(Status.PASS, "Title verified");
-	   
-         // Flush method will write the test in report- This is mandatory step  
-	  
-		
-		
+
+         // Flush method will write the test in report- This is mandatory step
+
+
+
 		// access to object
 		//1. extends
 		//2. create object
-		
+
 	lp = new landingPage(driver);
-		
+
 		//System.out.println(lp.getText1().getText());
 		Assert.assertEquals("aaaa", lp.getText2().getText());
 		//Assert.assertTrue(lp.getText1().getText().equals("abcd"));
@@ -159,37 +164,37 @@ public class testText extends Base{
 		logger.log(Status.SKIP, "test skipped");
 		//lggr.info("successfully valideted");
 		  extent.flush();
-		
+
 	}
 	@Test
 	public void headerTest2() throws IOException
 	{
 		 ExtentHtmlReporter reporter=new ExtentHtmlReporter("C://test//test1.html");
-			
+
          // Create object of ExtentReports class- This is main class which will create report
 	    ExtentReports extent = new ExtentReports();
-	    
+
          // attach the reporter which we created in Step 1
 	    extent.attachReporter(reporter);
-	    
+
          // call createTest method and pass the name of TestCase- Based on your requirement
 	    ExtentTest logger=extent.createTest("zzzLoginTest");
-	    
+
          // log method will add logs in report and provide the log steps which will come in report
 	    logger.log(Status.INFO, "Login to amazon");
-	   
+
 	    logger.log(Status.PASS, "Title verified");
-	   
-         // Flush method will write the test in report- This is mandatory step  
-	  
-		
-		
+
+         // Flush method will write the test in report- This is mandatory step
+
+
+
 		// access to object
 		//1. extends
 		//2. create object
-		
+
 	lp = new landingPage(driver);
-		
+
 		//System.out.println(lp.getText1().getText());
 		Assert.assertEquals("aaaa", lp.getText2().getText());
 		//Assert.assertTrue(lp.getText1().getText().equals("abcd"));
@@ -202,37 +207,37 @@ public class testText extends Base{
 		logger.log(Status.SKIP, "test skipped");
 		//lggr.info("successfully valideted");
 		  extent.flush();
-		
+
 	}
 	@Test
 	public void headerTest3() throws IOException
 	{
 		 ExtentHtmlReporter reporter=new ExtentHtmlReporter("C://test//test1.html");
-			
+
          // Create object of ExtentReports class- This is main class which will create report
 	    ExtentReports extent = new ExtentReports();
-	    
+
          // attach the reporter which we created in Step 1
 	    extent.attachReporter(reporter);
-	    
+
          // call createTest method and pass the name of TestCase- Based on your requirement
 	    ExtentTest logger=extent.createTest("zzzLoginTest");
-	    
+
          // log method will add logs in report and provide the log steps which will come in report
 	    logger.log(Status.INFO, "Login to amazon");
-	   
+
 	    logger.log(Status.PASS, "Title verified");
-	   
-         // Flush method will write the test in report- This is mandatory step  
-	  
-		
-		
+
+         // Flush method will write the test in report- This is mandatory step
+
+
+
 		// access to object
 		//1. extends
 		//2. create object
-		
+
 	lp = new landingPage(driver);
-		
+
 		//System.out.println(lp.getText1().getText());
 		Assert.assertEquals("aaaa", lp.getText2().getText());
 		//Assert.assertTrue(lp.getText1().getText().equals("abcd"));
@@ -245,18 +250,18 @@ public class testText extends Base{
 		logger.log(Status.SKIP, "test skipped");
 		//lggr.info("successfully valideted");
 		  extent.flush();
-		
+
 	}
-	
-	
-	
+
+
+
 	@AfterTest
 	public void closeWindow() {
 		driver.close();
 		driver = null;
 	}
-	
-	
-	
-	
+
+
+
+
 }
